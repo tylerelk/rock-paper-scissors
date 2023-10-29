013f29e7128435c90e8472793755306cd9cec534
tylerelk <144300250+tylerelk@users.noreply.github.com>
2023-10-29 09:56:55 -0700

    add UI reports and messages

--- game.js
+++ game.js
@@ -9,6 +9,9 @@ const buttonPaper = document.querySelector('#paper');
 const buttonScissors = document.querySelector('#scissors');
 const playerScoreDisplay = document.querySelector('#player-score');
 const computerScoreDisplay = document.querySelector('#computer-score');
+const roundReport = document.querySelector('#report');
+const gameUpdate = document.querySelector('#message');
+const winner = document.querySelector('#winner');
 
 //Buttons create player selection
 
@@ -35,20 +38,47 @@ function playRound () {
         (playerSelection === 2 && computerSelection === 1)
     ) {
         playerScore++;
-        console.log('You win!');
+        roundReport.textContent = 'You win!';
     } else if (
         (playerSelection === 0 && computerSelection === 1) ||
         (playerSelection === 1 && computerSelection === 2) ||
         (playerSelection === 2 && computerSelection === 0)
     ) {
         computerScore++;
-        console.log('You lose!');
+        roundReport.textContent = 'You lose!';
     } else {
-        console.log('Tie!')
+        roundReport.textContent = 'Tie!';
     }
 
     playerScoreDisplay.textContent = playerScore;
     computerScoreDisplay.textContent = computerScore;
 
-    console.log('You chose ' + playerSelection + ' and Computer chose ' + computerSelection)
+    if (playerSelection === 0) {
+        playerSelection = 'ROCK';
+    } else if (playerSelection === 1) {
+        playerSelection = 'PAPER';
+    } else {
+        playerSelection = 'SCISSORS';
+    }
+
+    if (computerSelection === 0) {
+        computerSelection = 'ROCK';
+    } else if (computerSelection === 1) {
+        computerSelection = 'PAPER';
+    } else {
+        computerSelection = 'SCISSORS';
+    }
+
+    gameUpdate.textContent = ('You chose ' + playerSelection + ' and Computer chose ' + computerSelection)
+
+    if (computerScore === 5) {
+        winner.textContent = 'Computer Wins!';
+        playerScore = 0;
+        computerScore = 0;
+    }
+    if (playerScore === 5) {
+        winner.textContent = 'You win!';
+        playerScore = 0;
+        computerScore = 0;
+    }
 }
